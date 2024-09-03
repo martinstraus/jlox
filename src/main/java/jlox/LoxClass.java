bb5ec689ca075bc9557bff8bde38baeef46185e6
Martin Straus <martinstraus@gmail.com>
2024-09-02 22:39:16 -0300

    Added support for class constructors.

--- src/main/java/jlox/LoxClass.java
+++ src/main/java/jlox/LoxClass.java
@@ -1,14 +1,30 @@
 package jlox;
 
-public class LoxClass {
+import java.util.List;
+
+public class LoxClass implements LoxCallable {
     private final String name;
     
     LoxClass(String name) {
         this.name = name;
     }
     
+    String name() {
+        return name;
+    }
+    
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public Object call(Interpreter interpreter, List<Object> arguments) {
+        return new LoxInstance(this);
+    }
+
+    @Override
+    public int arity() {
+        return 0;
     }
 }
---
+++ src/main/java/jlox/LoxInstance.java
@@ -0,0 +1,15 @@
+package jlox;
+
+public class LoxInstance {
+
+    private final LoxClass klass;
+
+    public LoxInstance(LoxClass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String toString() {
+        return klass.name() + " instance";
+    }
+}
